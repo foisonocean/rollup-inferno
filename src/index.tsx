db1c2f6ed7d2c8ee15c8c65c29e835e31afd095d
foisonocean <hronro@gmail.com>
2018-07-25 18:17:19 +0800

    :sparkles: add inferno devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import { render } from 'inferno';
+import { initDevTools } from 'inferno-devtools';
 
 import { App } from 'app';
+
+if ((process.env.NODE_ENV as string) !== 'production') {
+  initDevTools();
+}
 
 render(<App />, document.getElementById('root'));
